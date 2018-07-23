29e60eb1241ceca172f3cd013e863e1459cc3746
tmenochet <tmenochet@gmail.com>
2018-07-24 00:07:01 +0200

    Add wildcard for URL enum

--- webscraper/spiders/waybackmachine.py
+++ webscraper/spiders/waybackmachine.py
@@ -5,6 +5,7 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 from scrapy_wayback_machine import WaybackMachineMiddleware
+
 from webscraper.items import SearchResultItem
 
 class WaybackMachineSpider(CrawlSpider):
@@ -19,20 +20,17 @@ class WaybackMachineSpider(CrawlSpider):
         allow=()
         deny=()
         self.rules = (
-            Rule(LinkExtractor(allow=allow, deny=deny), callback=self.parse_response),
+            Rule(LinkExtractor(allow=allow, deny=deny), callback=self.parse),
         )
 
         # parse the allowed domains and start urls
         self.allowed_domains = []
         self.start_urls = []
-        url_parts = domain.split('://')
-        unqualified_url = url_parts[-1]
-        url_scheme = url_parts[0] if len(url_parts) > 1 else 'http'
-        full_url = '{0}://{1}'.format(url_scheme, unqualified_url)
-        bare_domain = unqualified_url.split('/')[0]
-        self.allowed_domains.append(bare_domain)
-        self.start_urls.append(full_url)
-        super().__init__(**kwargs)
+        url = 'http://' + domain + '/*'
+        self.allowed_domains.append(domain)
+        self.start_urls.append(url)
+
+        super().__init__()
 
     def parse_start_url(self, response):
         # scrapy doesn't call the callbacks for the start urls by default,
@@ -42,9 +40,9 @@ class WaybackMachineSpider(CrawlSpider):
                 if rule.callback:
                     rule.callback(response)
 
-    def parse_response(self, response):
+    def parse(self, response):
         item = SearchResultItem()
-        item['url'] = response.url
+        item['url'] = response.meta['wayback_machine_url'].split("id_/")[1]
         time = response.meta['wayback_machine_time']
         item['timestamp'] = time.strftime(WaybackMachineMiddleware.timestamp_format)
         yield item
